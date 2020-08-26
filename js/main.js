796bad9eb9f24c49d4a9f5060309ff78e9d84b68
and-jef <69371516+and-jef@users.noreply.github.com>
2020-08-26 12:23:38 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,3 +147,52 @@
 
 
 }());
+
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.querySelector("a#myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Lire plus";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Lire moins";
+	  moreText.style.display = "inline";
+	}
+  }
+
+  function myFunction1() {
+	var dots = document.getElementById("dots1");
+	var moreText = document.getElementById("more1");
+	var btnText = document.querySelector("a#myBtn1");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Lire plus";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Lire moins";
+	  moreText.style.display = "inline";
+	}
+  }
+
+  function myFunction2() {
+	var dots = document.getElementById("dots2");
+	var moreText = document.getElementById("more2");
+	var btnText = document.querySelector("a#myBtn2");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Lire plus";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Lire moins";
+	  moreText.style.display = "inline";
+	}
+  }
+  
